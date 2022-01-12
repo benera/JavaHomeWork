94de66af1ae33e0481bc9714cc48527d62258172
Aleksandra Benera <aleksandra.benera@gmail.com>
2022-01-12 21:49:07 +0200

    task 15 complete

---
+++ src/com/pb/benera/hw15/ClientInterface.java
@@ -0,0 +1,74 @@
+package com.pb.benera.hw15;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class ClientInterface extends JFrame implements ActionListener {
+    private String serverIp = "127.0.0.1";
+    private int serverPort = 1777;
+    private final JTextArea textArea = new JTextArea ( );
+    private final JTextField name = new JTextField ("ЧАТИК");
+    private final JTextField input = new JTextField ( );
+    JLabel name2 = new JLabel ( );
+    String color256 = "\u001B[38;2;";
+
+
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater (() -> new ClientInterface ( ));
+
+    }
+
+
+    private ClientInterface() {
+        System.out.println ("Соединяемся с сервером " + serverIp + ":" + serverPort);
+        setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
+        setSize (600, 400);
+        setLocationRelativeTo (null);
+        setAlwaysOnTop (true);
+        textArea.setLineWrap (true);
+        add (textArea, BorderLayout.CENTER);
+        input.addActionListener (this);
+        add (input, BorderLayout.SOUTH);
+        add (name, BorderLayout.NORTH);
+        name.setForeground (Color.BLACK);
+        name.setBackground (Color.GREEN);
+        setVisible (true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+
+        Socket server = null;
+
+        try {
+            server = new Socket (serverIp, serverPort);
+            BufferedReader inServer = new BufferedReader (new InputStreamReader (server.getInputStream ( )));
+            PrintWriter outServer = new PrintWriter (server.getOutputStream ( ), true);
+            outServer.flush ( );
+            String msg = input.getText ( );
+            input.setText (null);
+            outServer.println (msg);
+            textArea.append ("ALEX:" + msg + "\n");
+            textArea.append (inServer.readLine ( ));
+            textArea.append ("\n");
+            if ("exit".equalsIgnoreCase (msg)) {
+                return;
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace ( );
+        }
+
+
+    }
+}
---
+++ src/com/pb/benera/hw15/Server.java
@@ -0,0 +1,63 @@
+package com.pb.benera.hw15;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    static class Handler implements Runnable {
+        private final Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+        @Override
+        public void run() {
+            String clientMessage;
+
+            try {
+                System.out.println (Thread.currentThread ( ).getName ( ) + ": Получен запрос от клиента");
+                BufferedReader in = new BufferedReader (new InputStreamReader (socket.getInputStream ( )));
+                PrintWriter out = new PrintWriter (socket.getOutputStream ( ), true);
+                System.out.println ("Ждем сообщений......");
+                while ((clientMessage = in.readLine ( )) != null) {
+                    if ("exit".equalsIgnoreCase (clientMessage)) {
+                        System.out.println (">>>>>>>>>>>>>>>>>>>>>>>>>>");
+                        System.out.println (Thread.currentThread ( ).getName ( ) + " " + "Конец переписке");
+                        out.println ("Сервер : Конец переписке");
+                        break;
+                    }
+                    out.println ("Сервер :" + LocalDateTime.now ( ) + " " + clientMessage);
+                    System.out.println ("Сервер печатает на консоль: " + clientMessage + "\n" + "Дата/время сообщения: " + LocalDateTime.now ( ));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace ( );
+            }
+        }
+
+        public static void main(String[] args) throws Exception {
+            System.out.println ("Старт сервера");
+            int serverPort = 1777;
+            ServerSocket serverSocket = new ServerSocket (serverPort);
+            System.out.println ("Сервер запущен на порту:" + serverPort);
+            ExecutorService threadPool = Executors.newFixedThreadPool (5);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept ( );
+                threadPool.submit (new Handler (clientSocket));
+
+
+            }
+
+        }
+
+
+    }
+}
